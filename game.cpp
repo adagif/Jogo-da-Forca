bd6e2b18a87cd09a33d942f1b73f2b5e5022282c
Maria Eduarda <mariaeduarda.computacao@gmail.com>
2021-05-05 23:19:30 -0300

    Adicionando novas funções

--- game.cpp
+++ game.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -9,12 +10,55 @@ void limpaTela(){
 	system("clear");
 }
 
+string PalavraAleatoria(){
+	int x;
+	srand((unsigned)time(NULL));
+
+	x = rand()%3;
+	string valores[3] = { "chocolate", "batata", "docinho" };
+
+	//cout << valores[x];
+
+	return valores[x];
+
+}
+
+int tamanho(string palavra){
+	int x;
+
+	x = palavra.size();
+	return x;
+}
+
+string palavraOculta(int tam){
+	int i;
+	string p;
+
+	for(i=0;i<tam;i++){
+		p += "_ ";
+	}
+	return p;
+}
+
+void jogoIndividual(){
+	string p,po;
+	int tam;
+
+	p = PalavraAleatoria();
+	tam = tamanho(p);
+
+	po = palavraOculta(tam);
+	cout << po;
+}
+
+
+
 void menu(){
 	int opcao = 0;
 
 	while(opcao!=3){
 		limpaTela();
-		cout<< "\nBem vindo ao jogo";
+		cout<< "\n\nBem vindo ao jogo";
 		cout<< "\n1 - Jogar";
 		cout<< "\n2 - Sobre";
 		cout<< "\n3 - Sair";
@@ -23,7 +67,9 @@ void menu(){
 
 		switch(opcao){
 			case 1:
-				cout<< "Jogo iniciado";
+				cout<< "Jogo iniciado\n";
+				jogoIndividual();
+
 				limpaTela();
 				break;
 			case 2:
